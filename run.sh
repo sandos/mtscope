7a839fdfb6fb6a2942fd3a1e082a25165c1f75da
John B <sopues@gmail.com>
2026-09-09 21:33:44 +0200

    Enable clean DB

--- run.sh
+++ run.sh
@@ -3,4 +3,9 @@ set -eu
 
 TOPIC="$(bashio::config 'topic')"
 RETENTION_DAYS="$(bashio::config 'retention_days')"
+RESET_DATABASE="$(bashio::config 'reset_database')"
+if bashio::var.true "$RESET_DATABASE"; then
+	bashio::log.warning "Removing stored monitor data"
+	rm -f /data/meshat-monitor.db /data/meshat-monitor.db-shm /data/meshat-monitor.db-wal
+fi
 exec meshat-monitor --database /data/meshat-monitor.db --topic "$TOPIC" --retention-days "$RETENTION_DAYS" --http-port 8099 --web-root /usr/local/share/meshat-monitor
